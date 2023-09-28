17264d12dd30a906e9e646cb8477727aba27402e
appletri <tri_duc_lam@hotmail.com>
2023-09-28 10:50:00 -0600

    updated about me and techstack section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,20 +1,27 @@
 import pic from '../assets/profile.jpg'
+import bg from '../assets/clement-helardot-95YRwf6CNw8-unsplash.jpg'
 function AboutMe() {
   return (
     <div className="about-me">
-      <div className="profile">
-        <img src={pic} alt="Profile picture of Tri Lam"></img>
+      <div className='about-me-bg'>
+        <div className='bg-filter'></div>
+        <img src={bg} alt="photo of JavaScript in progress by Clement Helardot from Unsplash.com"></img>
       </div>
-      <div className="about-section">
-        <h1>Hello! My name is Tri Lam</h1>
-        <p>Full-Stack Web Developer</p>
-        <div className="links">
-          <a href="https://github.com/Appletri" target='_blank'>
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
-          </a>
-          <a href="https://www.linkedin.com/in/triduclam" target='_blank'>
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg" />
-          </a>
+      <div className='profile-container'>
+        <div className="profile">
+          <img src={pic} alt="Profile picture of Tri Lam"></img>
+        </div>
+        <div className="about-section">
+          <h1>T R I | L A M</h1>
+          <p>Full-Stack Dev | Digital Artist | Designer | Tech Enthusiest</p>
+          <div className="links">
+            <a href="https://github.com/Appletri" target='_blank'>
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
+            </a>
+            <a href="https://www.linkedin.com/in/triduclam" target='_blank'>
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg" />
+            </a>
+          </div>
         </div>
       </div>
 
